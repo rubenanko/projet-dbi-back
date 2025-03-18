a224b059fd57634bbd4f1372d0a4ebbfd57f5726
rubenanko <rubenanko@gmail.com>
2025-03-18 10:56:20 +0100

    update: appel api correction

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ import os
 
 
 #flask
-# app = Flask(__name__)
-# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 #config
 CONFIG_PATH = os.path.split(os.path.realpath(__file__))[0] + "\\..\\config.ini"
@@ -19,43 +19,42 @@ CONFIG.read(CONFIG_PATH)
 #openai
 openai.api_key = CONFIG["OPENAI"]["API_KEY_MAIN"]
 
-#génération de la correction
-#création de thread
-thread = openai.beta.threads.create()
 
-#création du prompt
-message = openai.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content="Quel est le nom exact des documents ?"
-)
 
-#lancement de la run
-run = openai.beta.threads.runs.create(thread_id=thread.id,assistant_id=CONFIG["OPENAI"]["ASSISTANT_ID_CORRECTION"])
 
-#attente du traitement de la run
-wait = True
-while wait:
-    run_status = openai.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-    wait = (run_status.status == "completed")
-    time.sleep(1)
+@app.route('/api/v1/correction',methods=['POST'])
+def correction():
+    question = request.json["question"]
+    answer = request.json["answer"]
+    #génération de la correction
+    #création de thread
+    thread = openai.beta.threads.create()
 
-#attente de la réponse de l'assistant
-wait = True
-while wait:
+    #création du prompt
+    message = openai.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=f"La question posée est : \n\n{question}\n\nMa réponse est : \n\n{answer}\n\n"
+    )
+
+    #lancement de la run
+    run = openai.beta.threads.runs.create(thread_id=thread.id,assistant_id=CONFIG["OPENAI"]["ASSISTANT_ID_CORRECTION"])
+
+    #attente du traitement de la run et de la réponse de l'assistant
+    waitForRunCompletion = True
+    waitForAnswer = True
+    while waitForRunCompletion or waitForAnswer:
+        if waitForRunCompletion:
+            run_status = openai.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+            waitForRunCompletion = (run_status.status == "completed")
+        elif waitForAnswer:
+                messages = openai.beta.threads.messages.list(thread_id=thread.id)
+                waitForAnswer = not [msg for msg in messages.data if msg.role == "assistant"]     
+        time.sleep(1)
+
+    #traitement du résultat
     messages = openai.beta.threads.messages.list(thread_id=thread.id)
-    wait = [msg for msg in messages.data if msg.role == "assistant"]
-    time.sleep(1)
 
-#traitement du résultat
-messages = openai.beta.threads.messages.list(thread_id=thread.id)
-
-for msg in messages.data:
-    print(msg.content[0].text.value)
-
-#asynchrone ?
-#créer un endpoint d'initiation du thread
-
-# @app.route('/api/v1/auth/signin',methods=['GET'])
-# def login():
-#     return "salut les gars"
+    # for msg in messages.data:
+    #     print(msg.content[0].text.value)
+    return { "assistant" : messages.data[0].content[0].text.value }
